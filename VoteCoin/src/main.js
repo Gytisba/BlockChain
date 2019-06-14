a9dde45ccbc1285e072f2b8f29f9601a0b03f9a7
DominykasJurkus <dominykas.jurkus1@gmail.com>
2019-06-14 07:49:01 +0300

    Update main.js
    Commented out some of the redundant code.

--- VoteCoin/src/main.js
+++ VoteCoin/src/main.js
@@ -9,30 +9,30 @@ const myKey = ec.keyFromPrivate('7c4c45907dec40c91bab3480c39032e90049f1a44f3e18c
 const myWalletAddress = myKey.getPublic('hex');
 
 // Create new instance of Blockchain class
-const savjeeCoin = new Blockchain();
+const voteCoin = new Blockchain();
 
 // Create a transaction & sign it with your key
 const tx1 = new Transaction(myWalletAddress, 'address2', 100);
 tx1.signTransaction(myKey);
-savjeeCoin.addTransaction(tx1);
+voteCoin.addTransaction(tx1);
 
 // Mine block
-savjeeCoin.minePendingTransactions(myWalletAddress);
+voteCoin.minePendingTransactions(myWalletAddress);
 
 // Create second transaction
-const tx2 = new Transaction(myWalletAddress, 'address1', 50);
-tx2.signTransaction(myKey);
-savjeeCoin.addTransaction(tx2);
+//const tx2 = new Transaction(myWalletAddress, 'address1', 50);
+//tx2.signTransaction(myKey);
+//voteCoin.addTransaction(tx2);
 
 // Mine block
-savjeeCoin.minePendingTransactions(myWalletAddress);
+//voteCoin.minePendingTransactions(myWalletAddress);
 
-console.log();
-console.log(`Balance of xavier is ${savjeeCoin.getBalanceOfAddress(myWalletAddress)}`);
+//console.log();
+//console.log(`Balance of xavier is ${voteCoin.getBalanceOfAddress(myWalletAddress)}`);
 
 // Uncomment this line if you want to test tampering with the chain
-// savjeeCoin.chain[1].transactions[0].amount = 10;
+// voteCoin.chain[1].transactions[0].amount = 10;
 
 // Check if the chain is valid
-console.log();
-console.log('Blockchain valid?', savjeeCoin.isChainValid() ? 'Yes' : 'No');
+//console.log();
+//console.log('Blockchain valid?', voteCoin.isChainValid() ? 'Yes' : 'No');
